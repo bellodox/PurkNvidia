47cf613f454f0d887cb98bc0fd4659ec988ff68d
rbuddy101 <rbuddy101@aol.com>
2018-05-25 08:05:14 -0700

    Add files via upload

---
+++ PurkMiner/Mine @ Fairpool.bat
@@ -0,0 +1,3 @@
+:loop
+ccminer-x64 -a wildkeccak -o stratum+tcp://mine.purk.fairpool.xyz:2244 -u CHANGE-TO-WALLET-ADDRESS -p x -k http://purk.fairpool.xyz/static/scratchpad.bin -l scratchpad.bin -i 22 -l 500x620 -s 2 -r 1
+goto loop
