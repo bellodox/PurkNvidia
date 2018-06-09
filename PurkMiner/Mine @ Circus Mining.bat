dffd8361f471f65c100e693b156ed5c317e62cb9
rbuddy101 <rbuddy101@aol.com>
2018-06-08 17:29:40 -0700

    Update Mine @ Circus Mining.bat

--- PurkMiner/Mine @ Circus Mining.bat
+++ PurkMiner/Mine @ Circus Mining.bat
@@ -1,3 +1,3 @@
 :loop
-ccminer-x64 -a wildkeccak -o stratum+tcp://purk.circusmining.com:7777 -u CHANGE-TO-WALLET-ADDRESS -p x -k http://purk.circusmining.com/scratchpad.bin -l scratchpad.bin -i 22 -l 500x620 -s 2 -r 1
+ccminer-x64 -a wildkeccak -o stratum+tcp://purk.circusmining.com:7777 -u CHANGE-TO-WALLET-ADDRESS -p x -k https://purk.circusmining.com/scratchpad.bin -l scratchpad.bin -i 22 -l 500x620 -s 2 -r 1
 goto loop
