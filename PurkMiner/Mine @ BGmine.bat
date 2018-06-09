7ef6d70998283460a0f0db648d7f91766e21cedb
rbuddy101 <rbuddy101@aol.com>
2018-06-08 17:29:24 -0700

    Update Mine @ BGmine.bat

--- PurkMiner/Mine @ BGmine.bat
+++ PurkMiner/Mine @ BGmine.bat
@@ -1,3 +1,3 @@
 :loop
-ccminer-x64 -a wildkeccak -o stratum+tcp://bgmine.me:6666 -u CHANGE-TO-WALLET-ADDRESS -p x -k http://bgmine.me/purk/scratchpad.bin -l scratchpad.bin -i 22 -l 500x620 -s 2 -r 1
+ccminer-x64 -a wildkeccak -o stratum+tcp://bgmine.me:6666 -u CHANGE-TO-WALLET-ADDRESS -p x -k https://bgmine.me/purk/scratchpad.bin -l scratchpad.bin -i 22 -l 500x620 -s 2 -r 1
 goto loop
