d3f108ec2a6e71dbf154f3c4a1e71860a0c10755
rbuddy101 <rbuddy101@aol.com>
2018-06-08 17:30:36 -0700

    Update Mine @ PurkProject.bat

--- PurkMiner/Mine @ PurkProject.bat
+++ PurkMiner/Mine @ PurkProject.bat
@@ -1,3 +1,3 @@
 :loop
-ccminer-x64 -a wildkeccak -o stratum+tcp://pool.purkproject.com:5555 -u CHANGE-TO-WALLET-ADDRESS -p x -k http://pool.purkproject.com/scratchpad.bin -l scratchpad.bin -i 22 -l 500x620 -s 2 -r 1
+ccminer-x64 -a wildkeccak -o stratum+tcp://pool.purkproject.com:5555 -u CHANGE-TO-WALLET-ADDRESS -p x -k https://pool.purkproject.com/scratchpad.bin -l scratchpad.bin -i 22 -l 500x620 -s 2 -r 1
 goto loop
